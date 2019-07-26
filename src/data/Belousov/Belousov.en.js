267583d637cf7372efedf25d46c9cd97413ad46d
vadikmash <vadikmash@gmail.com>
2019-07-27 02:44:48 +0300

    fix: .JSON data files to .js

---
+++ src/data/Belousov/Belousov.en.js
@@ -0,0 +1,81 @@
+exports.frontmatter = {
+  path: '/Belousov',
+  name: 'Oleg Pavlovich Belousov',
+  date: 'November 13, 1938 - March 26, 2009',
+  img: '../assets/images/belousov.jpg',
+  videoId: '_bw31-y7Dg',
+  vita: 'Belarusian Soviet director, screenwriter, operator. Honored art worker of the Belarusian SSR (1974).',
+  authorOfTheDay: 'false',
+  lang: 'en',
+  birthPlace: 'Slutsk, Minsk region',
+  timelineData: [
+    {
+      date: '1966',
+      text:
+                'Graduated from Belarusian State University.',
+    },
+    {
+      date: '1968 - 1973',
+      text:
+                'Worked as a literary employee of the newspaper «Literature and art».',
+    },
+    {
+      date: '1975—1989',
+      text:
+                'Founder and artistic director of the Belarusfilm cartoon studio.',
+    },
+    {
+      date: '1989-1995',
+      text:
+                'Founder and artistic director of animation studios, including the studio «ABC».',
+    },
+    {
+      date: '1995',
+      text:
+                'Video studio vice president «Frame-2».',
+    },
+    {
+      date: '1996',
+      text:
+                'Member of the Belarusian Union of Journalists, the Belarusian Union of Cinematographers, the Belarusian Union of Artists and the Belarusian Popular Front «Revival»',
+    },
+    {
+      date: '2005 - 2006',
+      text:
+                'Author of «My city» (2005) and  collection of poems «Kazantip notebook» (2006)',
+    },
+  ],
+
+  work: [
+    {
+      title: 'Very old man with huge wings',
+      date: '1990',
+      img: '../assets/images/belousov_very_old_man.jpg',
+    },
+    {
+      title: 'Cube',
+      date: '1988',
+      img: '../assets/images/belousov_cube.jpg',
+    },
+    {
+      title: 'Rook of despair',
+      date: '1987',
+      img: '../assets/images/belousov_ladya_otchainiya.jpg',
+    },
+    {
+      title: 'A Song about aurochs',
+      date: '1982',
+      img: '../assets/images/belousov_pesnia_o_zubre.jpg',
+    },
+    {
+      title: 'Tale of a funny clown',
+      date: '1980',
+      img: '../assets/images/belousov_o_veselom_kloune.jpg',
+    },
+    {
+      title: 'Nesterka',
+      date: '1980',
+      img: '../assets/images/belousov_nesterka.jpg',
+    },
+  ],
+};
---
+++ src/data/Marukhin/Marukhin.en.js
@@ -0,0 +1,85 @@
+exports.frontmatter = {
+  path: '/Marukhin',
+  name: 'Yuri Alexandrovich Marukhin',
+  date: 'July 13, 1938 - December 20, 2001',
+  img: '../assets/images/marukhin.jpg',
+  videoId: '9SCgy424OKc',
+  vita: 'Belarusian Soviet director, screenwriter, operator. Honored art worker of the Belarusian SSR (1974).',
+  authorOfTheDay: 'false',
+  lang: 'en',
+  birthPlace: 'Fedorovka village, region of Ryazan',
+  timelineData: [
+    {
+      date: '1960',
+      text:
+                'Worked as cameraman on the «Belarusfilm» film studio.',
+    },
+    {
+      date: '1962',
+      text:
+                'Graduated from the Faculty of Cameraman of the All-Union State Institute of Cinematography in Moscow (Leonid Kosmatov workshop).',
+    },
+    {
+      date: '1984',
+      text:
+                'Artistic film director.',
+    },
+    {
+      date: '2000 - 2001',
+      text:
+                'Chairman of the board of the Belarusian Union of Cinematographers.',
+    },
+    {
+      date: '1981',
+      text:
+                'Received the Order of the Badge of Honor.',
+    },
+    {
+      date: '1997, 2000',
+      text:
+                'Winner of the reception of the Ministry of Culture of Belarus and the Parliamentary Assembly of the Union of Belarus and Russia.',
+    },
+    {
+      date: '2007',
+      text:
+                'Michurinsk freeman.',
+    },
+  ],
+  work: [
+    {
+      title: 'Darling',
+      date: '1965',
+      img: '../assets/images/marukhin_darling(1965).jpg',
+    },
+    {
+      title: 'Eastern Corridor',
+      date: '1968',
+      img: '../assets/images/marukhin_eastern-corridor(1966).jpg',
+    },
+    {
+      title: 'Wait for me, Ann',
+      date: '1969',
+      img: '../assets/images/marukhin_wait_me_Ann(1969).jpg',
+    },
+    {
+      title: 'Black Sun',
+      date: '1970',
+      img: '../assets/images/marukhin_black_sun(1970).jpg',
+    },
+    {
+      title: 'Lions tomb',
+      date: '1971',
+      img: '../assets/images/marukhin_lions tomb(1971).jpg',
+    },
+    {
+      title: 'Chronicle of the night',
+      date: '1972',
+      img: '../assets/images/marukhin_сhronicle_of_the_night(1972).jpg',
+    },
+    {
+      title: 'Starting point',
+      date: '1979',
+      img: '../assets/images/marukhin_starting_point(1979).jpg',
+    },
+  ],
+};
