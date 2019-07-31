cd53c2963a2a895e4aa44a6b63c178b32535a172
IrynaZhadzinets <iryna.zhadzinets@gmail.com>
2019-07-31 22:48:31 +0300

    fix: timeline warning

--- src/components/author/timeLine.js
+++ src/components/author/timeLine.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import id from 'react-id-generator';
 import 'react-vertical-timeline-component/style.min.css';
 
 class VerticalTimelines extends React.Component {
   render() {
     const TimeLineItems = this.props.timelineData.map(timeLineItem => (
       <VerticalTimelineElement
+        key={id()}
         date={timeLineItem.date}
       >
         <p>{timeLineItem.text}</p>
