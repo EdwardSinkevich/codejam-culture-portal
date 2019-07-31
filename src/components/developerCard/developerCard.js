35fbc20f4a3a564bb7ea7ec33e60c05c2486c628
Nikita Nemov <nikitanemov1990@gmail.com>
2019-07-31 23:07:18 +0300

    fix: fix eslint problems

--- src/components/developerCard/developerCard.js
+++ src/components/developerCard/developerCard.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -17,65 +16,89 @@ const useStyles = makeStyles({
   },
   anchor: {
     textDecoration: 'none',
-    display: 'block'
+    display: 'block',
   },
   contribution: {
     padding: '10px',
+    width: '100%',
+    border: 'none',
     backgroundColor: '#008080',
     textAlign: 'center',
     color: 'white',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   contributionBlock: {
     height: '505px',
-    width: '230px'
-  }
+    width: '230px',
+  },
 });
 
-export default function DeveloperCard({ image, link, gitname, contribution }) {
+export default function DeveloperCard({
+  image, link, gitname, contribution,
+}) {
   const classes = useStyles();
   const [isContributionOpen, setContributionState] = useState(
-    false
-);
+    false,
+  );
 
   return (
     <Card className={classes.card}>
       {isContributionOpen ? (
-      <CardActionArea className={classes.contributionBlock}>
-        <Typography>
-          <ul>
-            {contribution.map(task => (
-              <li>{task}</li>
-            ))}
-          </ul>
-        </Typography>
-      </CardActionArea>
+        <CardActionArea className={classes.contributionBlock}>
+          <Typography>
+            <ul>
+              {contribution.map(task => (
+                <li>{task}</li>
+              ))}
+            </ul>
+          </Typography>
+        </CardActionArea>
       ) : (
-      <CardActionArea>
-        <a className={classes.anchor} href={link} target="_blank">
-          <CardMedia
-            component="img"
-            alt={gitname}
-            height="320"
-            image={image}
-            title={gitname}
-          />
-          <CardContent>
-            <Typography className={classes.content} gutterBottom variant="h6">
-            <div><svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></div>
-            <div>{gitname}</div>
-            </Typography>
-          </CardContent>
-        </a>
-      </CardActionArea>)}
-      <CardActionArea>
-          <a 
-            className={`${classes.anchor} ${classes.contribution}`} 
-            onClick={() => setContributionState(!isContributionOpen)}
-          >
-            {isContributionOpen ? 'Hide Contribution' : 'Show Contribution'}
+        <CardActionArea>
+          <a className={classes.anchor} href={link} target="_blank">
+            <CardMedia
+              component="img"
+              alt={gitname}
+              height="320"
+              image={image}
+              title={gitname}
+            />
+            <CardContent>
+              <Typography className={classes.content} gutterBottom variant="h6">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </div>
+                <div>{gitname}</div>
+              </Typography>
+            </CardContent>
           </a>
+        </CardActionArea>
+      )}
+      <CardActionArea>
+        <button
+          className={classes.contribution}
+          onClick={() => setContributionState(!isContributionOpen)}
+          type="submit"
+        >
+          {isContributionOpen ? 'Hide Contribution' : 'Show Contribution'}
+        </button>
       </CardActionArea>
     </Card>
   );
 }
+
+DeveloperCard.propTypes = {
+  image: PropTypes.string,
+  link: PropTypes.string,
+  gitname: PropTypes.string,
+  contribution: PropTypes.array,
+};
+
+DeveloperCard.defaultProps = {
+  image: '',
+  link: '',
+  gitname: '',
+  contribution: [],
+};
--- src/components/developersTeam/developersTeam.js
+++ src/components/developersTeam/developersTeam.js
@@ -17,63 +17,66 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function DevelopersTeam() {
-  const [spacing, setSpacing] = useState(2);
+  const [spacing] = useState(2);
   const classes = useStyles();
   const [developersState, setDevelopersState] = useState([
-    { gitHubName: 'dobrynanikitich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] },
-    { gitHubName: 'EdwardSinkevich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] },
-    { gitHubName: 'IrynaZhadzinets', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] },
-    { gitHubName: 'scepanenka', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] },
-    { gitHubName: 'vadikmash', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] },
-    { gitHubName: 'dmakarevich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'] }
+    {
+      gitHubName: 'dobrynanikitich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
+    {
+      gitHubName: 'EdwardSinkevich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
+    {
+      gitHubName: 'IrynaZhadzinets', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
+    {
+      gitHubName: 'scepanenka', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
+    {
+      gitHubName: 'vadikmash', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
+    {
+      gitHubName: 'dmakarevich', gitHubImage: '', gitHubLink: '', contribution: ['EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE', 'EXAMPLE'],
+    },
   ]);
 
 
   useEffect(() => {
-    let tempDevelopersState;
     async function getUsers(names) {
-      let jobs = [];
+      const jobs = [];
 
-      for(let name of names) {
-        let job = fetch(`https://api.github.com/users/${name.gitHubName}?client_id=52489756c9417a4f520d&client_secret=a7d3531dbea35461e57cf0fd810c52a9acf2e630`).then(
-          successResponse => {
-            if (successResponse.status != 200) {
+      names.forEach((name) => {
+        const job = fetch(`https://api.github.com/users/${name.gitHubName}?client_id=52489756c9417a4f520d&client_secret=a7d3531dbea35461e57cf0fd810c52a9acf2e630`)
+          .then((successResponse) => {
+            if (successResponse.status !== 200) {
               return null;
-            } else {
-              return successResponse.json().then(json => {
-                const newDev = {...name, gitHubImage: json.avatar_url, gitHubLink: json.html_url};
-                return newDev;
-              });
             }
-          },
-          failResponse => {
-            return null;
-          }
-        );
+            return successResponse.json().then((json) => {
+              const newDev = { ...name, gitHubImage: json.avatar_url, gitHubLink: json.html_url };
+              return newDev;
+            });
+          });
         jobs.push(job);
-      }
+      });
 
       const results = await Promise.all(jobs);
       return results;
     }
 
-     getUsers([...developersState]).then(data => {
-      setDevelopersState(data);
-    }
-    );
-
+    getUsers([...developersState]).then(data => setDevelopersState(data));
   }, []);
 
   return (
     <Grid container className={classes.root} spacing={1}>
       <Grid item xs={12}>
         <Grid container justify="center" spacing={spacing}>
-          {developersState.map(({ gitHubName, gitHubImage, gitHubLink, contribution }) => (
-              <Grid key={gitHubName} item>
-                <DeveloperCard image={gitHubImage} link={gitHubLink} gitname={gitHubName} contribution={contribution}/>
-              </Grid>
-            )
-          )}
+          {developersState.map(({
+            gitHubName, gitHubImage, gitHubLink, contribution,
+          }) => (
+            <Grid key={gitHubName} item>
+              <DeveloperCard image={gitHubImage} link={gitHubLink} gitname={gitHubName} contribution={contribution} />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Grid>
