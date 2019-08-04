9c8f38162c9cd96e632a118e8fc60d85545cd2b3
Edvard Sinkevich <mejik-yo@mail.ru>
2019-08-04 05:04:43 +0300

    fix: delete layout component from 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { withI18next } from 'gatsby-plugin-i18next';
-import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
+  <div>
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
 );
 
 
